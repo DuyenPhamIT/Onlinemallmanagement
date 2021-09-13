8badf1a4c60914a16359e8626197ad996b60a3fc
banghehe <phamduyentba@gmail.com>
2021-09-13 23:20:37 +0700

    first commit

---
+++ Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,22 @@
+﻿using OnlineMallManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineMallManagement.Areas.Admin.Controllers
+{
+    [Authorize]
+    public class DashboardController : Controller
+    {
+        private DBOnlineMallEntities dbContext = new DBOnlineMallEntities();
+        [Authorize(Roles = "Admin,User,Customer")]
+        public ActionResult Index()
+        {
+            HttpContext.Application["favicon"] = dbContext.Configs.Find(15).value;
+            HttpContext.Application["logo"] = dbContext.Configs.Find(16).value;
+            return View();
+        }
+    }
+}
